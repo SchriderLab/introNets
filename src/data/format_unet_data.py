eaee18710dd8bb4bdb898f0458e5516e597ef9f7
Dylan Ray <ddray@email.unc.edu>
2021-08-25 20:00:13 -0400

    stuff

--- src/data/format_unet_data.py
+++ src/data/format_unet_data.py
@@ -17,6 +17,18 @@ import h5py
 from seriate import seriate
 from scipy.spatial.distance import pdist
 
+from scipy.sparse import coo_matrix
+from scipy.sparse.csgraph import min_weight_full_bipartite_matching
+from scipy.spatial.distance import cdist, pdist, squareform
+from scipy.optimize import linear_sum_assignment
+import torch
+
+from scipy.linalg import svdvals
+from scipy.sparse.linalg import eigs
+from sklearn.metrics import pairwise_distances
+
+import matplotlib.pyplot as plt
+
 def seriate_x(x):
     Dx = pdist(x, metric = 'cosine')
     Dx[np.where(np.isnan(Dx))] = 0.
@@ -24,8 +36,34 @@ def seriate_x(x):
 
     return x[ix], ix
 
+def seriate_y(x):
+    x = x.T
+    
+    Dx = pdist(x, metric = 'cosine')
+    Dx[np.where(np.isnan(Dx))] = 0.
+    ix = seriate(Dx, timeout = 0)
+
+    return x[ix].T, ix
+
+def seriate_y_spectral(x):
+    x = x.T
+    
+    C = pairwise_distances(x)
+    C[np.where(np.isnan(C))] = 0.
+
+    C = np.diag(C.sum(axis = 1)) - C
+    print('computing spectra...')
+    _, v = eigs(C, k = 2, which = 'SM')
+
+    f = v[:,1]
+    ix = np.argsort(np.real(f))
+
+    x = x[ix,:]
+    
+    return x.T, ix
+
 class Formatter(object):
-    def __init__(self, x, y, shape = (2, 128, 128), pop_sizes = [150, 156], sorting = None):
+    def __init__(self, x, y, shape = (2, 128, 256), pop_sizes = [150, 156], sorting = None):
         # list of x and y arrays
         self.x = x
         self.y = y
@@ -58,20 +96,39 @@ class Formatter(object):
             y1 = y[x1_indices, :]
             y2 = y[x2_indices, :]
             
-            if self.sorting == 'seriation_match':
-                x1, x1_indices = seriate_x(x1)
-                y1 = y1[x1_indices, :]
+            x1, i1 = seriate_x(x1)
+            x2, i2 = seriate_x(x2)
+        
+            y1 = y1[i1, :]
+            y2 = y2[i2, :]
+            
+            y = np.vstack([y1, y2])
+            x = np.vstack([x1, x2])
+            
+            x, ii = seriate_y_spectral(x)
+            
+            x1 = x[:128, :]
+            x2 = x[128:, :]
+            
+            y = y[:,ii]
+            y1 = y[:128, :]
+            y2 = y[128:, :]
                 
-                x2, x2_indices = seriate_x(x2)
-                y2 = y2[x1_indices, :]
+            ii = np.where(x.sum(axis = 0) > 0)
+            x1 = np.squeeze(x1[:,ii])
+            x2 = np.squeeze(x2[:,ii])
+            y2 = np.squeeze(y2[:,ii])
+            
+            print(x1.shape, y2.shape, x2.shape)
             
             six = np.random.choice(range(x1.shape[1] - self.n_sites))
             
             x = np.array([x1[:,six:six + self.n_sites], x2[:, six:six + self.n_sites]])
-            y = np.array([y1[:,six:six + self.n_sites], y2[:, six:six + self.n_sites]])
+            #y = np.array([y1[:,six:six + self.n_sites], y2[:, six:six + self.n_sites]])
+            y2 = y2[:, six:six + self.n_sites]
             
             X.append(x)
-            Y.append(y)
+            Y.append(np.expand_dims(1 - y2, 0))
             
         return X, Y
             
