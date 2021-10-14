18bc412b918b22ac38d560cd235d161a2a4fde81
Dylan Ray <ddray@email.unc.edu>
2021-10-14 17:03:01 -0400

    debugging

--- src/data/format_unet_data.py
+++ src/data/format_unet_data.py
@@ -157,7 +157,7 @@ def main():
     chunk_size = int(args.chunk_size)
     
     msFiles = sorted(glob.glob(os.path.join(args.idir, '*.txt')))
-    ancFiles = sorted(glob.glob(os.path.join(args.idir, '*.anc')))
+    ancFiles = sorted([u for u in glob.glob(os.path.join(args.idir, '*.anc')) if not ('.1.' in u or '.alt.' in u)])
     
     counter = 0
 
