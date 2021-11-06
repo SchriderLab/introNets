a42ef9ca2d2631ad12d2d620c569196a4b23a7c4
Dylan Ray <ddray@email.unc.edu>
2021-11-06 19:02:35 -0400

    trying transfer learning after bug fix

--- src/models/train_gcn_unet_transfer.py
+++ src/models/train_gcn_unet_transfer.py
@@ -52,7 +52,7 @@ class TransferModel(nn.Module):
         self.conv = list(model.children())[1]
         self.transform = nn.Sequential(*list(list(model.children())[-2].children())[:-1])
         
-        self.out = nn.Linear(1024, 150)
+        self.out = nn.Linear(2048, 150)
         
         self.activation = nn.ReLU()
         
