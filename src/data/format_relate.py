fcc595664df38346faa8bbe93ed1401ff3e511f5
Dylan Ray <ddray@email.unc.edu>
2021-11-21 19:03:30 -0500

    updated to mpi version

--- src/data/format_relate.py
+++ src/data/format_relate.py
@@ -10,10 +10,10 @@ import copy
 import h5py
 import numpy as np
 
+from mpi4py import MPI
+
 import matplotlib
 matplotlib.use('Agg')
-
-
 
 # use this format to tell the parsers
 # where to insert certain parts of the script
@@ -40,10 +40,14 @@ def parse_args():
 def main():
     args = parse_args()
     
+    # configure MPI
+    comm = MPI.COMM_WORLD
+    
     idirs_relate = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if 'relate' in u])
     idirs = sorted([os.path.join(args.idir, u) for u in os.listdir(args.idir) if not ('relate' in u)])
     
-    ofile = h5py.File(args.ofile, 'w')
+    if comm.rank == 0:
+        ofile = h5py.File(args.ofile, 'w')
     
     dnsps = []
     
@@ -57,109 +61,139 @@ def main():
         
         anc_files = [os.path.join(idir_relate, u) for u in os.listdir(idir_relate) if u.split('.')[-1] == 'anc']
         
-        for ix in range(len(anc_files)):
-            ifile = os.path.join(idir, '{}.npz'.format(anc_files[ix].split('/')[-1].split('.')[0]))
-            ifile = np.load(ifile)
-            
-            x = ifile['x']
-            y = ifile['y']
-            pos = ifile['positions']
-            
-            snps = []
-            anc_file = open(anc_files[ix], 'r')
-            
-            lines = anc_file.readlines()[2:]
-            
-            for ij in range(len(lines)):
-                line = lines[ij]
+        comm.Barrier()
+        
+        if comm.rank != 0:
+            for ix in range(comm.rank - 1, len(anc_files), comm.size - 1):
+                ifile = os.path.join(idir, '{}.npz'.format(anc_files[ix].split('/')[-1].split('.')[0]))
+                ifile = np.load(ifile)
                 
-                nodes = []
-                parents = []
-                lengths = []
-                n_mutations = []
-                regions = []
+                x = ifile['x']
+                y = ifile['y']
+                pos = ifile['positions']
                 
-                edges = []
+                comm.send([ix, x, y, pos], dest = 0)
                 
-                # new tree
-                line = line.replace(':', ' ').replace('(', '').replace(')', '').replace('\n', '')
-                line = line.split(' ')[:-1]
-    
-                start_snp = int(line[0])
-                snps.append(start_snp)
+                snps = []
+                anc_file = open(anc_files[ix], 'r')
                 
-                for j in range(2, len(line), 5):
-                    nodes.append((j - 1) // 5)
+                lines = anc_file.readlines()[2:]
+                
+                for ij in range(len(lines)):
+                    line = lines[ij]
                     
-                    parents.append(int(line[j]))
-                    lengths.append(float(line[j + 1]))
-                    n_mutations.append(float(line[j + 2]))
-                    regions.append((int(line[j + 3]), int(line[j + 4])))
+                    nodes = []
+                    parents = []
+                    lengths = []
+                    n_mutations = []
+                    regions = []
                     
-                    edges.append((parents[-1], nodes[-1]))
+                    edges = []
                     
-                G = nx.DiGraph()
-                G.add_edges_from(edges)
-                
-                current_day_nodes = []
-                
-                data = dict()
-                
-                # find the nodes which have no out degree
-                for node in G.nodes():
-                    d = G.out_degree(node)
+                    # new tree
+                    line = line.replace(':', ' ').replace('(', '').replace(')', '').replace('\n', '')
+                    line = line.split(' ')[:-1]
+        
+                    start_snp = int(line[0])
+                    snps.append(start_snp)
                     
-                    if d == 0:
-                        current_day_nodes.append(node)
+                    for j in range(2, len(line), 5):
+                        nodes.append((j - 1) // 5)
                         
-                for node in current_day_nodes[:len(current_day_nodes) // 2]:
-                    data[node] = [0., 1., 0., 0.]
-                
-                for node in current_day_nodes[len(current_day_nodes) // 2:]:
-                    data[node] = [0., 0., 1., 0.]
-                    
-                t = 0.
-                
-                nodes = copy.copy(current_day_nodes)
-                while len(data.keys()) < len(G.nodes()):
-                    _ = []
-                    for node in nodes:
-                        for j in range(len(edges)):
-                            if edges[j][-1] == node:
-                                p = edges[j][0]
-                                break
+                        parents.append(int(line[j]))
+                        lengths.append(float(line[j + 1]))
+                        n_mutations.append(float(line[j + 2]))
+                        regions.append((int(line[j + 3]), int(line[j + 4])))
                         
-                        data[p] = [data[node][0] + lengths[j], 0., 0., 1.]
-                        _.append(p)
+                        edges.append((parents[-1], nodes[-1]))
                         
-                    nodes = copy.copy(_)
+                    G = nx.DiGraph()
+                    G.add_edges_from(edges)
                     
-                X = []
-                for node in sorted(data.keys()):
-                    X.append(data[node])
+                    current_day_nodes = []
                     
-                X = np.array(X)
-                X[:,0] /= np.max(X[:,0])
+                    data = dict()
+                    
+                    # find the nodes which have no out degree
+                    for node in G.nodes():
+                        d = G.out_degree(node)
+                        
+                        if d == 0:
+                            current_day_nodes.append(node)
+                            
+                    for node in current_day_nodes[:len(current_day_nodes) // 2]:
+                        data[node] = [0., 1., 0., 0.]
+                    
+                    for node in current_day_nodes[len(current_day_nodes) // 2:]:
+                        data[node] = [0., 0., 1., 0.]
+                        
+                    t = 0.
+                    
+                    nodes = copy.copy(current_day_nodes)
+                    while len(data.keys()) < len(G.nodes()):
+                        _ = []
+                        for node in nodes:
+                            for j in range(len(edges)):
+                                if edges[j][-1] == node:
+                                    p = edges[j][0]
+                                    break
+                            
+                            data[p] = [data[node][0] + lengths[j], 0., 0., 1.]
+                            _.append(p)
+                            
+                        nodes = copy.copy(_)
+                        
+                    X = []
+                    for node in sorted(data.keys()):
+                        X.append(data[node])
+                        
+                    X = np.array(X)
+                    X[:,0] /= np.max(X[:,0])
+                    
+                    edges = np.array(edges).T
+                    
+                    comm.send([ix, ij, edges, X, regions, n_mutations], dest = 0)
+                    
+                comm.send([ix, snps], dest = 0)
+            else:
+                n_received = 0
+                index = dict()
                 
-                edges = np.array(edges).T
-            
-                ofile.create_dataset('{0}/graph/{1}/xg'.format(counter, ij), data = X, compression = 'lzf')
-                ofile.create_dataset('{0}/graph/{1}/edge_index'.format(counter, ij), data = edges.astype(np.int32), compression = 'lzf')
-                ofile.create_dataset('{0}/graph/{1}/regions'.format(counter, ij), data = np.array(regions).astype(np.int32), compression = 'lzf')
-                ofile.create_dataset('{0}/graph/{1}/n_mutations'.format(counter, ij), data = np.array(n_mutations), compression = 'lzf')
+                while n_received < len(anc_files):
                 
-            
-            ofile.create_dataset('{0}/x'.format(counter), data = x.astype(np.uint8), compression = 'lzf')
-            ofile.create_dataset('{0}/y'.format(counter), data = y.astype(np.uint8), compression = 'lzf')
-            ofile.create_dataset('{0}/positions'.format(counter), data = pos, compression = 'lzf')
-            ofile.create_dataset('{0}/break_points'.format(counter), data = np.array(snps, dtype = np.int32), compression = 'lzf')
-            
-            counter += 1
-            
-            ofile.flush()
-
-    
+                    v = comm.recv(source = MPI.ANY_SOURCE)
                 
+                    if v[0] in index.keys():
+                        ii = index[v[0]]
+                    else:
+                        index[v[0]] = counter
+                        counter += 1
+                        
+                        ii = index[v[0]]
+                
+                    if len(v) == 6:
+                        ix, ij, edges, X, regions, n_mutations = v
+                        
+                        ofile.create_dataset('{0}/graph/{1}/xg'.format(ii, ij), data = X, compression = 'lzf') # time, population
+                        ofile.create_dataset('{0}/graph/{1}/edge_index'.format(ii, ij), data = edges.astype(np.int32), compression = 'lzf') # indices of branches (i.e. graph edges as index pairs)
+                        ofile.create_dataset('{0}/graph/{1}/regions'.format(ii, ij), data = np.array(regions).astype(np.int32), compression = 'lzf')
+                        ofile.create_dataset('{0}/graph/{1}/n_mutations'.format(ii, ij), data = np.array(n_mutations), compression = 'lzf')
+                        
+                    elif len(v) == 4:
+                        _, x, y, pos = v
+                        
+                        ofile.create_dataset('{0}/x'.format(ii), data = x.astype(np.uint8), compression = 'lzf')
+                        ofile.create_dataset('{0}/y'.format(ii), data = y.astype(np.uint8), compression = 'lzf')
+                        ofile.create_dataset('{0}/positions'.format(counter), data = pos, compression = 'lzf')
+                    elif len(v) == 2:
+                        _, snps = v
+                        
+                        ofile.create_dataset('{0}/break_points'.format(ii), data = np.array(snps, dtype = np.int32), compression = 'lzf')
+                        
+                        n_received += 1
+                    
+                    ofile.flush()
+                    
                 
             
     ofile.close()
