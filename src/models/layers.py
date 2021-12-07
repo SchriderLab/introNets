23e5a24430062fa6347f1e33c02b6f189c79e92f
Dylan Ray <ddray@email.unc.edu>
2021-12-07 16:32:41 -0500

    stuff

--- src/models/layers.py
+++ src/models/layers.py
@@ -104,8 +104,8 @@ class GCNUNet_i3(nn.Module):
                                         layer_type = layer_type, heads = n_heads, return_attention_weights = return_attention_weights)
         n = n_features * (n_layers - 1)
         
-        self.transform = nn.Sequential(nn.Linear(2 * n, 4096), nn.LayerNorm(4096), nn.ReLU(), 
-                                       nn.Linear(4096, 4096), nn.LayerNorm(4096), nn.ReLU(), 
+        self.transform = nn.Sequential(nn.Linear(2 * n, 4096), nn.LayerNorm(4096), nn.ELU(), nn.Dropout(0.25),
+                                       nn.Linear(4096, 4096), nn.LayerNorm(4096), nn.ELU(), nn.Dropout(0.25),
                                        nn.Linear(4096, n_classes))
         
         self.activation = nn.ReLU()
@@ -633,20 +633,19 @@ class LSTMClassifier(nn.Module):
 class VGGBlock(nn.Module):
     def __init__(self, in_channels, middle_channels, out_channels):
         super().__init__()
-        self.relu = nn.ReLU(inplace=True)
+        self.act = nn.ELU()
         self.conv1 = nn.Conv2d(in_channels, middle_channels, 3, padding=1)
-        self.bn1 = nn.BatchNorm2d(middle_channels)
+        self.bn1 = nn.InstanceNorm2d(middle_channels)
         self.conv2 = nn.Conv2d(middle_channels, out_channels, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.bn2 = nn.InstanceNorm2d(out_channels)
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.bn1(out)
-        out = self.relu(out)
 
         out = self.conv2(out)
         out = self.bn2(out)
-        out = self.relu(out)
+        out = self.act(out)
 
         return out
 
@@ -757,6 +756,8 @@ class NestedUNet(nn.Module):
         self.pool = nn.MaxPool2d(2, 2)
         self.up = nn.Upsample(scale_factor = 2, mode='bilinear', align_corners=True)
 
+        self.dropout = nn.Dropout2d(0.2)
+
         self.conv0_0 = VGGBlock(input_channels, nb_filter[0], nb_filter[0])
         self.conv1_0 = VGGBlock(nb_filter[0], nb_filter[1], nb_filter[1])
         self.conv2_0 = VGGBlock(nb_filter[1], nb_filter[2], nb_filter[2])
@@ -790,21 +791,25 @@ class NestedUNet(nn.Module):
         x0_0 = self.conv0_0(input)
         x1_0 = self.conv1_0(self.pool(x0_0))
         x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0)], 1))
+        x0_1 = self.dropout(x0_1)
 
         x2_0 = self.conv2_0(self.pool(x1_0))
         x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0)], 1))
         x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1)], 1))
+        x0_2 = self.dropout(x0_2)
 
         x3_0 = self.conv3_0(self.pool(x2_0))
         x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0)], 1))
         x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1)], 1))
         x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2)], 1))
+        x0_3 = self.dropout(x0_3)
 
         x4_0 = self.conv4_0(self.pool(x3_0))
         x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0)], 1))
         x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1)], 1))
         x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2)], 1))
         x0_4 = self.conv0_4(torch.cat([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)], 1))
+        x0_4 = self.dropout(x0_4)
 
         if self.deep_supervision:
             output1 = self.final1(x0_1)
