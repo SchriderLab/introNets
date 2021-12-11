381cd64510a36c3695623a16b962ad8ad17f36c1
Dylan Ray <ddray@email.unc.edu>
2021-12-11 17:36:51 -0500

    stuff

--- src/models/dgl_layers.py
+++ src/models/dgl_layers.py
@@ -5,10 +5,88 @@ import torch
 
 from torch_geometric.nn import MessageNorm
 
+class Res1dEncoder(nn.Module):
+    def __init__(self, in_channels = 1, n_res_layers = 4):
+        super(Res1dEncoder, self).__init__()
+        
+        channels = [1, 24, 48, 96, 288]
+        
+        self.convs = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        
+        for ix in range(n_res_layers):
+            self.convs.append(Res1dBlock(channels[ix], channels[ix + 1] // 3, 3))
+            self.norms.append(nn.Sequential(nn.InstanceNorm2d(channels[ix + 1]), nn.Dropout2d(0.05)))
+            
+        self.out = nn.Conv2d(channels[-1], channels[-1] // 2, 1, 1)
+            
+    def forward(self, x):
+        for conv, norm in zip(self.convs, self.norms):
+            x = norm(conv(x)).relu_()
+            
+        x = self.out(x)
+            
+        return x
+    
+class Res1dBlockUp(nn.Module):
+    def __init__(self, in_channels, out_channels, n_layers, 
+                             k = 3, pooling = 'max', up = False):
+        super(Res1dBlockUp, self).__init__()
+        
+        # pass each pop through their own convolutions
+        self.convs = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        
+        for ix in range(n_layers):
+            self.convs.append(nn.Conv2d(in_channels, out_channels, (1, 3), 
+                                        stride = (1, 1), padding = (0, (k + 1) // 2 - 1), bias = False))
+
+            self.norms.append(nn.Sequential(nn.InstanceNorm2d(out_channels), nn.Dropout2d(0.1)))
+            
+            in_channels = out_channels
+        
+        self.up = nn.Upsample(scale_factor=(1,2), mode='bicubic', align_corners=True)
+        
+        self.activation = nn.ReLU()
+        
+    def forward(self, x, edge_index, edge_attr, batch):
+        x = self.up(x)
+        
+        batch_size, n_channels, n_ind, n_sites = x.shape
+        xs = [self.norms[0](self.convs[0](x))]
+        
+        for ix in range(1, len(self.norms)):
+            xs.append(self.convs[ix](xs[-1]))        
+            xs[-1] = self.norms[ix](xs[-1] + xs[-2])
+            
+        x = torch.cat([x, self.activation(torch.cat(xs, dim = 1))], dim = 1)
+        
+        return x
+    
+    
+class Res1dDecoder(nn.Module):
+    def __init__(self, in_channels = 1, n_res_layers = 4):
+        super(Res1dEncoder, self).__init__()
+        
+        channels = [288, 96, 48, 24, 1]
+        
+        self.convs = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        
+        for ix in range(n_res_layers):
+            self.convs.append(Res1dBlockUp(channels[ix], channels[ix + 1] // 3, 3))
+            self.norms.append(nn.Sequential(nn.InstanceNorm2d(channels[ix + 1]), nn.Dropout2d(0.05)))
+            
+    def forward(self, x):
+        for conv, norm in zip(self.convs, self.norms):
+            x = norm(conv(x))
+            
+        return x
+
 class TreeLSTMCell(nn.Module):
     def __init__(self, h_size):
         super(TreeLSTMCell, self).__init__()
-        self.U_iou = nn.Linear(2 * h_size, 3 * h_size, bias=False)
+        self.U_iou = nn.Linear(2 * h_size, 3 * h_size, bias = False)
         self.b_iou = nn.Parameter(torch.zeros(1, 3 * h_size))
         self.U_f = nn.Linear(2 * h_size, 2 * h_size)
         self.norm_iou = MessageNorm(True)
@@ -52,29 +130,13 @@ class TreeLSTM(nn.Module):
     def __init__(self, in_channels = 1, conv_channels = 48, n_cycles = 2, h_size = 128, dropout = 0.1):
         super(TreeLSTM, self).__init__()
         
-        n_convs = n_cycles * 2
-        
-        self.init_conv = Res1dBlock((1,), 1, 3, pooling = None)
-        self.init_norm = nn.InstanceNorm2d(3)
-        
-        self.convs = nn.ModuleList()
-        self.down_convs = nn.ModuleList()
-        self.norms = nn.ModuleList()
-        
-        channels = in_channels
+        self.encoder = Res1dEncoder()
+        self.decoder = Res1dDecoder()
         
         # at each iteration of topological message passing, convolve the node features across
-        # the chromosome.
-        for ix in range(n_convs):
-            self.convs.append(Res1dBlock((channels,), conv_channels // 3, n_layers = 3, pooling = None))
-            self.down_convs.append(nn.Conv2d(conv_channels, 3, 1, bias = False))
-            self.norms.append(nn.InstanceNorm2d(conv_channels))
-            
-            channels = 3
             
         self.dropout = nn.Dropout(dropout)
-        self.out = nn.Conv2d(n_convs * 3 + 1, 1, 1)
-        self.cell = TreeLSTMCell(h_size)
+        self.cell = TreeLSTMCell(256)
 
     def forward(self, g, h, c):
         """Compute tree-lstm prediction given a batch.
@@ -100,7 +162,7 @@ class TreeLSTM(nn.Module):
         g.ndata['x'] = (g.ndata['x']).view(ind, 1, 1, s)
         
         # transform all the nodes via 1d convolution
-        g.ndata['iou'] = self.init_norm(self.init_conv(g.ndata['x'])).view(ind, 3 * s)
+        g.ndata['iou'] = self.encoder(g.ndata['x'])
 
         g.ndata['h'] = h
         g.ndata['c'] = c
@@ -111,7 +173,8 @@ class TreeLSTM(nn.Module):
                             reduce_func=self.cell.reduce_func,
                             apply_node_func=self.cell.apply_node_func, reverse = reverse)
         g.ndata['iou'] = g.ndata['iou'].relu_()
-        xs = [g.ndata['iou'].view(ind, 3, 1, s)]
+
+        
                           
         for ix in range(1, len(self.convs)):
             reverse = (not reverse)
