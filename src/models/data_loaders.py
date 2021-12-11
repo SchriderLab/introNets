526dcb91d1fa623358a784d703b1ac27cfb5648a
Dylan Ray <ddray@email.unc.edu>
2021-12-11 18:21:16 -0500

    stuff

--- src/models/data_loaders.py
+++ src/models/data_loaders.py
@@ -101,7 +101,7 @@ class DGLDataGenerator(object):
         
         edge_attr = np.concatenate([xg[edges[0,:]] - xg[edges[1,:]], n_mutations], axis = 1)
         
-        return torch.FloatTensor(x), torch.FloatTensor(y), torch.LongTensor(edges), torch.FloatTensor(y_mask)
+        return torch.FloatTensor(x), torch.FloatTensor(y), torch.LongTensor(edges), torch.BoolTensor(y_mask == 1)
                                        
     
     def get_batch(self, val = False):
