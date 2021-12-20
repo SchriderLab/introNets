b60f982cae799d530d5e97b0c879180a7df8479e
Dylan Ray <ddray@email.unc.edu>
2021-12-20 04:15:17 -0500

    1d equi conv stuff

--- src/models/layers_1d.py
+++ src/models/layers_1d.py
@@ -1058,6 +1058,8 @@ class Eq1dConv(nn.Module):
         x = self.conv(self.norm(x))
         x = filtered_lrelu.filtered_lrelu(x=x, fu = self.up_filter, fd = self.down_filter, b = self.bias.to(x.dtype),
             up=2, down=2, padding=self.padding, gain=1., clamp=self.conv_clamp)
+        
+        return x
 
 class GCNConvNet_beta(nn.Module):
     def __init__(self, in_channels = 1, depth = 7, pred_pop = 1):
@@ -1090,7 +1092,6 @@ class GCNConvNet_beta(nn.Module):
         xc = self.convs[0](x)
         xg = torch.flatten(xc.transpose(1, 2), 2, 3).flatten(0, 1)
         xg = self.norms[0](self.gcns[0](x, edge_index, edge_attr))
-        print(xg.shape)
         
         xg = to_dense_batch(xg, batch)[0]
         xg = xg.reshape(batch_size, ind, 1, sites).transpose(1, 2)
