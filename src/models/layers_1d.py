fa570d7cda65e57308e941875b53ccc299114dd8
Dylan Ray <ddray@email.unc.edu>
2021-12-20 04:07:45 -0500

    1d equi conv stuff

--- src/models/layers_1d.py
+++ src/models/layers_1d.py
@@ -1041,7 +1041,7 @@ class Eq1dConv(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, (1, k), 
                                         stride = (1, 1), padding = (0, (k + 1) // 2 - 1), bias = False)
         self.register_buffer('up_filter', design_lowpass_filter())
-        self.register_buffer('down_filter', design_lowpass_filter(cutoff = 128 - 1, width = WIDTH * 2))
+        self.register_buffer('down_filter', design_lowpass_filter(cutoff = 128 - 1, width = WIDTH * 2, fs = 256))
         self.bias = torch.nn.Parameter(torch.zeros([self.out_channels]))
         
         self.conv_clamp = 64
