ed4893f108ce68df45c970a795542f3f00856fe3
Dylan Ray <ddray@email.unc.edu>
2021-12-20 00:56:30 -0500

    added script to convert binary data to continuous

--- src/data/convert_to_continuous.py
+++ src/data/convert_to_continuous.py
@@ -52,6 +52,12 @@ def main():
     #val_keys = list(ifile['val'].keys())
 
     x = np.cumsum(np.array(ifile['train']['0']['x_0']), axis = 2) * 4 * np.pi
+    mask = np.zeros(x.shape)
+    ix = np.where(np.diff(x) != 0)
+    ix[-1] += 1
+    mask[ix] = 1
+    
+    x[mask == 0] = 0
 
     x[:,:,0] = 2 * np.pi
     
@@ -64,8 +70,13 @@ def main():
         
         t = np.array(range(np.max(ix)))
         
-        x[k,:len(t)] = interp1d(ix, x[k,ix], kind = 'cubic')(t)
-        
+        if len(ix) > 3:
+            x[k,:len(t)] = interp1d(ix, x[k,ix], kind = 'cubic')(t)
+        elif len(ix) > 2:
+            x[k,:len(t)] = interp1d(ix, x[k,ix], kind = 'quadratic')(t)
+        else:
+            x[k,:len(t)] = interp1d(ix, x[k,ix], kind = 'quadratic')(t)
+            
     plt.imshow(x)
     plt.savefig('test_output.png', dpi = 100)
     plt.close()
