fa0e3fc9b0813dbee7a57ddc8b0c12b47502a3a4
Dylan Ray <ddray@email.unc.edu>
2022-01-01 21:24:24 -0500

    v4 i4 of segmentation net, sigmoid attention instead of softmax, + regular convolution at the end to introduce needed high frequencies...I think

--- src/models/layers_1d.py
+++ src/models/layers_1d.py
@@ -1331,7 +1331,7 @@ class GCNUNet_delta(nn.Module):
             
         in_channels = 16
         
-        self.pre_out = Res1dBlock(in_channels * 2 + up_channels[-1], in_channels * 2 + up_channels[-1], 1, pooling = None)
+        self.pre_out = Res1dBlock((in_channels * 2 + up_channels[-1], ), in_channels * 2 + up_channels[-1], 1, pooling = None)
         self.out = nn.Conv2d(in_channels * 2 + up_channels[-1], 1, 1, 1, bias = False)
         
         self.out_down1 = nn.Conv2d(in_channels + up_channels[-1], in_channels // 4, 1, 1)
