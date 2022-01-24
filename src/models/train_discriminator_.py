b52b3f541606800b043b527ae010eb996c436138
Dylan Ray <ddray@email.unc.edu>
2022-01-24 01:53:47 -0500

    stuff

--- src/models/train_discriminator_.py
+++ src/models/train_discriminator_.py
@@ -67,6 +67,61 @@ def parse_args():
     # ${odir_del_block}
 
     return args
+from sklearn.metrics import accuracy_score, confusion_matrix
+import pandas as pd
+# use this format to tell the parsers
+# where to insert certain parts of the script
+# ${imports}
+
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def cm_analysis(y_true, y_pred, filename, labels, ymap=None, figsize=(10,10)):
+    """
+    Generate matrix plot of confusion matrix with pretty annotations.
+    The plot image is saved to disk.
+    args: 
+      y_true:    true label of the data, with shape (nsamples,)
+      y_pred:    prediction of the data, with shape (nsamples,)
+      filename:  filename of figure file to save
+      labels:    string array, name the order of class labels in the confusion matrix.
+                 use `clf.classes_` if using scikit-learn models.
+                 with shape (nclass,).
+      ymap:      dict: any -> string, length == nclass.
+                 if not None, map the labels & ys to more understandable strings.
+                 Caution: original y_true, y_pred and labels must align.
+      figsize:   the size of the figure plotted.
+    """
+    if ymap is not None:
+        y_pred = [ymap[yi] for yi in y_pred]
+        y_true = [ymap[yi] for yi in y_true]
+        labels = [ymap[yi] for yi in labels]
+    cm = confusion_matrix(y_true, y_pred)
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=labels, columns=labels)
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+    sns.heatmap(cm, annot=annot, fmt='', ax=ax)
+    plt.savefig(filename)
+    plt.close()
 
 def main():
     args = parse_args()
@@ -138,7 +193,7 @@ def main():
             losses.append(loss.item())
 
             # compute accuracy in CPU with sklearn
-            y_pred = np.exp(y_pred.detach().cpu().numpy())
+            y_pred = y_pred.detach().cpu().numpy()
             y = y.detach().cpu().numpy()
 
             y_pred = np.argmax(y_pred, axis=1)
@@ -154,6 +209,9 @@ def main():
             ij += 1
 
         model.eval()
+        
+        Y = []
+        Y_pred = []
 
         val_losses = []
         val_accs = []
@@ -173,10 +231,13 @@ def main():
 
                 loss = criterion(y_pred, y)
                 # compute accuracy in CPU with sklearn
-                y_pred = np.exp(y_pred.detach().cpu().numpy())
+                y_pred = y_pred.detach().cpu().numpy()
                 y = y.detach().cpu().numpy()
 
                 y_pred = np.argmax(y_pred, axis=1)
+                
+                Y.extend(y)
+                Y_pred.extend(y)
 
                 # append metrics for this epoch
                 val_accs.append(accuracy_score(y, y_pred))
@@ -197,6 +258,8 @@ def main():
             min_val_loss = val_loss
             print('saving weights...')
             torch.save(model.state_dict(), os.path.join(args.odir, '{0}.weights'.format(args.tag)))
+            
+            cm_analysis(Y, Y_pred, os.path.join(args.odir, '{}_best.png'.format(args.tag)), ['AB', 'BA', 'none'])
 
             early_count = 0
         else:
