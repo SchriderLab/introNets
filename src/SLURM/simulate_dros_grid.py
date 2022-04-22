632cd2565ea38a6f2da9014d6702a8aa45d9a816
Dylan Ray <ddray@email.unc.edu>
2022-04-22 16:22:30 -0400

    stuff

---
+++ src/SLURM/simulate_dros_grid.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import os
+import argparse
+import logging
+
+import numpy as np
+
+# use this format to tell the parsers
+# where to insert certain parts of the script
+# ${imports}
+
+def parse_args():
+    # Argument Parser
+    parser = argparse.ArgumentParser()
+    # my args
+    parser.add_argument("--verbose", action = "store_true", help = "display messages")
+    parser.add_argument("--odir", default = "None")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+        logging.debug("running in verbose mode")
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if args.odir != "None":
+        if not os.path.exists(args.odir):
+            os.system('mkdir -p {}'.format(args.odir))
+            logging.debug('root: made output directory {0}'.format(args.odir))
+    # ${odir_del_block}
+
+    return args
+
+def main():
+    args = parse_args()
+
+    cmd = 'python3 src/data/simulate_msmodified.py --mt_range {0} --t_range {1} --odir {2} --n_jobs 250 --n_samples 100'
+    
+    st = np.array([0.5  , 0.625, 0.75 , 0.875, 1.   ])
+    mt = np.array([0.  , 0.05, 0.1 , 0.15, 0.2 ])
+    
+    for i in range(len(st) - 1):
+        for j in range(len(mt) - 1):
+            odir = os.path.join(args.odir, '{0}_{1}'.format(i, j))
+            
+            cmd_ = cmd.format('{0},{1}'.format(mt[j], mt[j + 1]), '{0},{1}'.format(st[i], st[i + 1]), odir)
+            os.system(cmd_)
+            
+    # ${code_blocks}
+
+if __name__ == '__main__':
+    main()
+
--- src/data/simulate_msmodified.py
+++ src/data/simulate_msmodified.py
@@ -6,6 +6,7 @@ import argparse
 import logging
 
 from data_functions import writeTbsFile
+import copy
 
 # this function creates an array for writing to text that has the ms parameters
 # from a CSV file produced via bootstrapped DADI runs
@@ -39,7 +40,7 @@ def parameters_df(df, ix, thetaOverRho, migTime, migProb, n):
     
     p = np.tile(np.array([theta, rho, nu1, nu2, alpha1, alpha2, 0, 0, T, T, migTime, 1 - migProb, migTime]), (n, 1)).astype(object)
     
-    return p, ll
+    return p, ll, Nref
 
 def parse_args():
     # Argument Parser
@@ -52,10 +53,13 @@ def parse_args():
     parser.add_argument("--ifile", default = "None", help = "CSV file of bootstrapped demographic estimates. only applicable for the drosophila case; --model dros")
     
     parser.add_argument("--model", default = "dros", help = "model you'd like to simulate. current options our 'archie' and 'dros'")
-    parser.add_argument("--direction", default = "ba", help = "directionality of migration. only applicable for the drosophila case; --model dros")
+    parser.add_argument("--direction", default = "ab", help = "directionality of migration. only applicable for the drosophila case; --model dros")
     parser.add_argument("--slurm", action = "store_true")
     
     parser.add_argument("--window_size", default = "50000", help = "size in base pairs of the region to simulate")
+    
+    parser.add_argument("--mt_range", default = "None")
+    parser.add_argument("--t_range", default = "None")
 
     parser.add_argument("--odir", default = "None", help = "directory for the ms output and logs to be written to")
     args = parser.parse_args()
@@ -77,10 +81,10 @@ def parse_args():
 def main():
     args = parse_args()
     
+    # the supported models for this script
     if not (args.model in ['dros', 'archie']):
         print('not a supported model for this script...')
         print("valid options for --model: 'archie', 'dros'")
-    
         return
     
     if args.model == 'dros':
@@ -101,19 +105,35 @@ def main():
                 
                 L = 10000
                 
-                P, ll = parameters_df(df, ix, 1., 0., 0., n)
-    
-                print(ll)
+                P, ll, Nref = parameters_df(df, ix, 1., 0., 0., n)
                 if ll > -2000:
                     odir = os.path.join(args.odir, 'iter{0:06d}'.format(counter))
                     counter += 1
                     
                     os.system('mkdir -p {}'.format(odir))
                     
-                    # replace mean migTime and the rest with a uniformly random distribution around it
-                    migTime = np.random.uniform(0., 0.1, (P.shape[0], ))
+                    if args.mt_range != "None":
+                        mt_range = tuple(list(map(int, args.mt_range.split(','))))
+                        
+                        # replace mean migTime and the rest with a uniformly random distribution around it
+                        migTime = np.random.uniform(mt_range[0], mt_range[1], (P.shape[0], ))
+                    
+                    
                     migProb = 1 - np.random.uniform(0., 1.0, (P.shape[0], ))
                     rho = np.random.uniform(0.1, 0.3, (P.shape[0], ))
+                    
+                    if args.t_range != "None":
+                        t_range = tuple(list(map(int, args.t_range.split(','))))
+                        
+                        T = copy.copy(P[:,-4])
+                        
+                        # rescale alpha1 and alpha2
+                        P[:,[4,5]] *= T
+                        
+                        P[:,-4] = np.random.uniform(t_range[0], t_range[1], (P.shape[0], ))
+                        P[:,-4] /= (4*Nref / 15.)
+                        
+                        P[:,[4,5]] /= P[:,-4]
                     
                     P[:,-1] = migTime * P[:,-4]
                     P[:,-3] = migTime * P[:,-4]
@@ -141,7 +161,8 @@ def main():
                     elif args.direction == 'none_wtrees':
                         cmd = "cd %s; %s %d %d -t tbs -r tbs %d -I 2 %d %d -n 1 tbs -n 2 tbs -eg 0 1 tbs -eg 0 2 tbs -ma x tbs tbs x -ej tbs 2 1 -en tbs 1 1 -seed tbs -T < %s | tee %s" % (odir, os.path.join(os.getcwd(), 'msdir/ms'), SIZE_A + SIZE_B, len(P), L, SIZE_A, SIZE_B, 'mig.tbs', 'mig.msOut')
                     
-                    cmd = "echo '{0}' && {0}".format(cmd)
+                    # print the command, do the command, gzip the outputs
+                    cmd = "echo '{0}' && {0} && gzip mig.msOut out.anc".format(cmd)
                     print('simulating via the recommended parameters...')
                     sys.stdout.flush()
                     
@@ -160,7 +181,7 @@ def main():
             
             cmd = "cd {0}; {1} 202 {2} -t {3} -r {4} {5} -I 4 100 100 1 1 g  -en 0 1 1  -es 0.05 1 {6} -ej 0.05 5 3  -ej 0.0625 2 1 -en 0.15 3 0.01 -en 0.153 3 1 -ej 0.175 4 3 -ej 0.3 3 1 -seeds {7} {8} {9} | tee mig.msOut".format(odir, os.path.join(os.getcwd(), 'msmodified/ms'), n, T, R, L, A, seeds[0], seeds[1], seeds[2])
         
-            cmd = "echo '{0}' && {0}".format(cmd)
+            cmd = "echo '{0}' && {0} && gzip mig.msOut out.anc".format(cmd)
             print('simulating via the recommended parameters...')
             sys.stdout.flush()
             
