6aa447cc90a0cbea1c590d46965856631d8e116a
Dylan Ray <ddray@email.unc.edu>
2022-05-09 16:16:53 -0400

    fixed params again...

--- src/SLURM/simulate_dros_grid.py
+++ src/SLURM/simulate_dros_grid.py
@@ -36,8 +36,8 @@ def main():
 
     cmd = 'python3 src/data/simulate_msmodified.py --mt_range {0} --t_range {1} --odir {2} --n_jobs 25 --n_samples 100 --slurm --ifile params.txt'
     
-    st = np.array([0.5  , 0.625, 0.75 , 0.875, 1.   ])
-    mt = np.linspace(45000, 105000, 5)
+    st = np.linspace(45000, 105000, 5)
+    mt = np.array([0.  , 0.05, 0.1 , 0.15, 0.2 ])
     
     for i in range(len(st) - 1):
         for j in range(len(mt) - 1):
