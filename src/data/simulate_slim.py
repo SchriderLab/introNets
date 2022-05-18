d40b21dd1349a452399b5fe7967c8fbf037e27d7
Dylan Ray <ddray@email.unc.edu>
2022-05-17 23:57:50 -0400

    cleaning up some more

--- src/data/simulate_slim.py
+++ src/data/simulate_slim.py
@@ -10,21 +10,23 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # my args
     parser.add_argument("--verbose", action = "store_true", help = "display messages")
-    parser.add_argument("--odir", default = "/proj/dschridelab/introgression_data/")
-    parser.add_argument("--n_jobs", default = "1000")
-    parser.add_argument("--n_replicates", default = "100")
+    parser.add_argument("--odir", default = "None", help = "output directory")
+    parser.add_argument("--n_jobs", default = "1000", help = "number of SLiM commands to run")
+    parser.add_argument("--n_replicates", default = "100", help = "number of simulation replicates to run per SLiM command")
 
     # simulation SLiM script
-    parser.add_argument("--slim_file", default = "src/SLiM/introg_bidirectional.slim")
+    parser.add_argument("--slim_file", default = "src/SLiM/introg_bidirectional.slim", 
+                        help = "location of .slim file (instructions for the demography and other parameters, etc.")
 
     # parameters for the simulation
-    parser.add_argument("--st", default = "4")
-    parser.add_argument("--mt", default = "0.25")
-    parser.add_argument("--mp", default = "1")
-    parser.add_argument("--phys_len", default = "3000")
-    parser.add_argument("--donor_pop", default="1")
+    parser.add_argument("--st", default = "4", help = "split time coefficient (see .slim file)")
+    parser.add_argument("--mt", default = "0.25", help = "migration time coefficient (see .slim file)")
+    parser.add_argument("--mp", default = "1", help = "migration probability (legacy isn't actually used...)")
+    parser.add_argument("--phys_len", default = "3000", help = "length of simulated chromosome in base pairs")
+    parser.add_argument("--donor_pop", default="1", help = "donor pop (0, 1, or 2 for bidirectional + any other value for no migration)")
+    parser.add_argument("--local", action="store_true", help = "whether to run locally (no SLURM)")
 
-    parser.add_argument("--n_per_pop", default = "64")
+    parser.add_argument("--n_per_pop", default = "64", help = "number of sampled individuals in each population")
 
     args = parser.parse_args()
 
@@ -52,16 +54,24 @@ def parse_args():
 def main():
     args = parse_args()
     
-    # scriptName, numReps, physLen, donorPop, introgLogFileName, nPerPop, splitTimeCoefficient, migrationTimeCoefficient, migrationProbability
-    cmd = 'sbatch --mem=4G -t 02:00:00 -o {10} --wrap "python3 src/data/runAndParseSlim.py {0} {1} {2} {3} {4} {5} {6} {7} {8} > {9} && gzip {4} {9}"'
+    if not args.local:
+        # scriptName, numReps, physLen, donorPop, introgLogFileName, nPerPop, splitTimeCoefficient, migrationTimeCoefficient, migrationProbability
+        cmd = 'sbatch --mem=4G -t 02:00:00 -o {10} --wrap "python3 src/data/runAndParseSlim.py {0} {1} {2} {3} {4} {5} {6} {7} {8} > {9} && gzip {4} {9}"'
+    else:
+        cmd = 'python3 src/data/runAndParseSlim.py {0} {1} {2} {3} {4} {5} {6} {7} {8} > {9} && gzip {4} {9}'
+
     
     for ix in range(int(args.n_jobs)):
         ofile_ms = os.path.join(args.odir, '{0:05d}.ms'.format(ix))
         ofile_introg = os.path.join(args.odir, '{0:05d}_introg.log'.format(ix))
         ofile_log = os.path.join(args.odir, '{0:05d}_introg.out'.format(ix))
         
-        cmd_ = cmd.format(args.slim_file, args.n_replicates, args.phys_len,
-                          args.donor_pop, ofile_introg, args.n_per_pop, args.st, args.mt, args.mp, ofile_ms, ofile_log)
+        if not args.local:
+            cmd_ = cmd.format(args.slim_file, args.n_replicates, args.phys_len,
+                              args.donor_pop, ofile_introg, args.n_per_pop, args.st, args.mt, args.mp, ofile_ms, ofile_log)
+        else:
+            cmd_ = cmd.format(args.slim_file, args.n_replicates, args.phys_len,
+                              args.donor_pop, ofile_introg, args.n_per_pop, args.st, args.mt, args.mp, ofile_ms)
         
         os.system(cmd_)
         
