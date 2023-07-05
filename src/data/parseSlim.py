28cc8e146f56fda5beb31ae6a64994716fbb269a
Dylan Ray <ddray@email.unc.edu>
2023-07-05 17:01:19 -0400

    added script to work with prewritten output

--- src/data/parseSlim.py
+++ src/data/parseSlim.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 
 import glob
-from runAndParseSlim import readSampleOutFromSlimRun, buildMutationPosMapping, removeMonomorphic, buildPositionsList, writeIntrogressedAlleles
+from runAndParseSlim import buildMutationPosMapping, removeMonomorphic, buildPositionsList, writeIntrogressedAlleles, addMutationsAndGenomesFromSample
 import gzip
 
 def emitMsEntry(positions, segsites, haps, numReps, out, isFirst=True):
@@ -26,6 +26,46 @@ def emitMsEntry(positions, segsites, haps, numReps, isFirst=True):
     print("positions: "+ " ".join([str(x) for x in positions]))
     for hap in haps:
         print("".join([str(x) for x in hap]))
+
+import sys        
+
+def readSampleOutFromSlimRun(output, numSamples, sampleSize1):
+    totSampleCount = 0
+    for line in output:
+        if line.startswith("Sampling at generation"):
+            totSampleCount += 1
+    samplesToSkip = totSampleCount-numSamples
+    #sys.stderr.write("found {} samples and need to skip {}\n".format(totSampleCount, samplesToSkip))
+
+    if numSamples != 1:
+        sys.exit("numSamples={} in 'readSampleOutFromSlimRun' but time series not currently supported. AARRRGGGHHHHH!!!!!!".format(numSamples))
+
+    mode = 0
+    samplesSeen = 0
+    locs = {}
+    genomes = []
+    for line in output:
+        if mode == 0:
+            if line.startswith("migProb") or line.startswith("migTime"):
+                sys.stderr.write(line+"\n")
+            if line.startswith("splitTime"):
+                splitTime = int(line.strip().split("splitTime: ")[1])
+            if line.startswith("migTime"):
+                migTime = int(line.strip().split("migTime: ")[1])
+            if line.startswith("Sampling at generation"):
+                samplesSeen += 1
+                if samplesSeen >= samplesToSkip+1:
+                    sampleText = []
+                    mode = 1
+        elif mode == 1:
+            if line.startswith("Done emitting sample"):
+                mode = 0
+                introgressedAlleles = addMutationsAndGenomesFromSample(sampleText, locs, genomes, sampleSize1)
+            else:
+                sampleText.append(line)
+        if "SEGREGATING" in line:
+            sys.stderr.write(line+"\n")
+    return locs, genomes, introgressedAlleles
 
 # use this format to tell the parsers
 # where to insert certain parts of the script
@@ -69,7 +109,7 @@ def main():
         msOutFileName = os.path.join(args.odir, ifile.split('/')[-1].replace('.out.gz', '.log'))
 
         ifile = gzip.open(ifile, 'r')
-        output = ifile.readlines()
+        output = [u.decode('utf-8') for u in ifile.readlines()]
         
         outF = open(introgLogFileName, "wt")
         outF_ms = open(msOutFileName, "wt")
