cf0ee66ca86590c7b8f539dc61690e95a0eda23b
Dylan Ray <ddray@email.unc.edu>
2023-07-05 17:11:08 -0400

    added an extra arg for selection

--- src/SLURM/create_toy_data.py
+++ src/SLURM/create_toy_data.py
@@ -23,6 +23,8 @@ def parse_args():
     parser.add_argument("--mp", default = "1")
     parser.add_argument("--phys_len", default = "3000")
     parser.add_argument("--donor_pop", default="1")
+    
+    parser.add_argument("--intro_s", default = "-1e-6")
 
     parser.add_argument("--n_per_pop", default = "64")
 
@@ -53,7 +55,7 @@ def main():
     args = parse_args()
     
     # scriptName, numReps, physLen, donorPop, introgLogFileName, nPerPop, splitTimeCoefficient, migrationTimeCoefficient, migrationProbability
-    cmd = 'sbatch --mem=4G -t 02:00:00 -o {10} --wrap "python3 src/data/runAndParseSlim.py {0} {1} {2} {3} {4} {5} {6} {7} {8} > {9} && gzip {4} {9}"'
+    cmd = 'sbatch --mem=4G -t 02:00:00 -o {10} --wrap "python3 src/data/runAndParseSlim.py {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} > {10} && gzip {4} {10}"'
     
     for ix in range(int(args.n_jobs)):
         ofile_ms = os.path.join(args.odir, '{0:05d}.ms'.format(ix))
@@ -61,7 +63,7 @@ def main():
         ofile_log = os.path.join(args.odir, '{0:05d}_introg.out'.format(ix))
         
         cmd_ = cmd.format(args.slim_file, args.n_replicates, args.phys_len,
-                          args.donor_pop, ofile_introg, args.n_per_pop, args.st, args.mt, args.mp, ofile_ms, ofile_log)
+                          args.donor_pop, ofile_introg, args.n_per_pop, args.st, args.mt, args.mp, args.intro_s, ofile_ms, ofile_log)
         
         os.system(cmd_)
         
